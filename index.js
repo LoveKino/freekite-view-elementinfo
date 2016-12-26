5527428005185c1221763373c2d6a9b3aeb67de8
loveKino <842914439@qq.com>
2016-12-26 16:58:14 +0800

    add more options

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ let {
     n, view
 } = require('kabanery');
 
+let {
+    reduce, map
+} = require('bolzano');
+
 module.exports = view(({
     nodeInfo,
     config
 }) => {
-    let attributes = config.state.styles;
     let {
         node
     } = nodeInfo;
@@ -16,9 +19,70 @@ module.exports = view(({
     let textContent = node.textContent;
 
     node.chosenStyle = node.chosenStyle || {};
-    // TODO color converter
+    node.chosenAttributes = node.chosenAttributes || {};
 
+    // TODO color converter
     return n('div', [
+        n('h4', 'tagName'),
+
+        n(`input type="checkbox" ${node.chosenTagName? 'checked=true': ''}`, {
+            onclick: (e) => {
+                node.chosenTagName = e.target.checked;
+            }
+        }),
+
+        n('div', {
+            style: {
+                display: 'inline-block'
+            }
+        }, node.tagName),
+
+        n('h4', 'path'),
+
+        n(`input type="checkbox" ${node.strictPath? 'checked=true': ''}`, {
+            onclick: (e) => {
+                node.strictPath = e.target.checked;
+            }
+        }),
+
+        n('div', {
+            style: {
+                display: 'inline-block'
+            }
+        }, showStrictPath(nodeInfo)),
+
+        Object.keys(node.attributes || {}).length && [
+            n('h4', 'attributes'),
+
+            // style part
+            n('ul', {
+                style: {
+                    backgroundColor: 'white'
+                }
+            }, map(node.attributes, (value, key) => {
+                return n('li class=field-line', [
+                    n(`input type="checkbox" ${node.chosenAttributes[key]? 'checked=true': ''}`, {
+                        onclick: (e) => {
+                            node.chosenAttributes[key] = e.target.checked;
+                        }
+                    }),
+
+                    n('label', {
+                        style: {
+                            width: 100,
+                            display: 'inline-block'
+                        }
+                    }, key),
+
+                    n('div', {
+                        style: {
+                            display: 'inline-block'
+                        }
+                    }, value + '')
+                ]);
+            }))
+        ],
+
         n('h4', 'captured styles'),
 
         // style part
@@ -26,7 +90,7 @@ module.exports = view(({
             style: {
                 backgroundColor: 'white'
             }
-        }, attributes.map((name) => {
+        }, config.state.styles.map((name) => {
             return n('li class=field-line', [
                 n(`input type="checkbox" ${node.chosenStyle[name]? 'checked=true': ''}`, {
                     onclick: (e) => {
@@ -64,3 +128,11 @@ module.exports = view(({
         }, textContent)
     ]);
 });
+
+let showStrictPath = (nodeInfo) => {
+    let pathStr = reduce(nodeInfo.path.slice(0, -1), (prev, item) => {
+        return `${item.tagName} [${item.index}]  > ${prev}`;
+    }, '');
+
+    return pathStr + nodeInfo.node.tagName;
+};
